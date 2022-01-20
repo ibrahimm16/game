0ba788f67aad3acc6d3479c145cfdefe8c68fc2c
Ibrahim Manfoud <ibrahimmanfoud@gmail.com>
2022-01-19 20:59:05 -0500

    first commit

---
+++ src/game/Game.java
@@ -0,0 +1,13 @@
+package game;
+
+public class Game {
+
+    private Game() {}
+
+    private void start() {}
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+}
